2d403fb3649f77c02be20d89b64d213b0e27f1e8
Hashim332 <hashim.rafiq332@gmail.com>
2025-08-16 23:49:35 +0100

    lesson 24: login & token on  bearer stuff on swagger

--- api/Controllers/AccountController.cs
+++ api/Controllers/AccountController.cs
@@ -6,7 +6,10 @@ using api.Dtos.Account;
 using api.Interfaces;
 using api.Models;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 namespace api.Controllers
 {
@@ -16,11 +19,49 @@ namespace api.Controllers
     {
         private readonly UserManager<AppUser> _userManager;
         private readonly ITokenService _tokenService;
+        private readonly SignInManager<AppUser> _signinManager;
 
-        public AccountController(UserManager<AppUser> userManager, ITokenService tokenService)
+        public AccountController(
+            UserManager<AppUser> userManager,
+            ITokenService tokenService,
+            SignInManager<AppUser> signInManager
+        )
         {
             _userManager = userManager;
             _tokenService = tokenService;
+            _signinManager = signInManager;
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(LoginDto loginDto)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var user = await _userManager.Users.FirstOrDefaultAsync(x =>
+                x.UserName == loginDto.Username.ToLower()
+            );
+
+            if (user == null)
+                return Unauthorized("Invalid username!");
+
+            var result = await _signinManager.CheckPasswordSignInAsync(
+                user,
+                loginDto.Password,
+                false
+            );
+
+            if (!result.Succeeded)
+                return Unauthorized("Username not found and/or password incorrect");
+
+            return Ok(
+                new NewUserDto
+                {
+                    UserName = user.UserName,
+                    Email = user.Email,
+                    Token = _tokenService.CreateToken(user),
+                }
+            );
         }
 
         [HttpPost("register")]
