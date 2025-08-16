3c777d93960b5e2ca511af3723b52f2c6c8d9dbc
Hashim332 <hashim.rafiq332@gmail.com>
2025-08-16 18:41:55 +0100

    lesson15: updating comments

--- api/Controllers/CommentController.cs
+++ api/Controllers/CommentController.cs
@@ -64,5 +64,22 @@ namespace api.Controllers
                 commentModel.ToCommentDto()
             );
         }
+
+        [HttpPut]
+        [Route("{id}")]
+        public async Task<IActionResult> Update(
+            [FromRoute] int id,
+            [FromBody] UpdateCommentRequestDto updateDto
+        )
+        {
+            var comment = await _commentRepo.UpdateAsync(id, updateDto.ToCommentFromUpdate());
+
+            if (comment == null)
+            {
+                return NotFound("Comment not found");
+            }
+
+            return Ok(comment.ToCommentDto());
+        }
     }
 }
