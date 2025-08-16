081831ee8f4f276abead8e991a4c73f42517696b
Hashim332 <hashim.rafiq332@gmail.com>
2025-08-16 22:37:24 +0100

    lesson 22: adding user registrations

---
+++ api/Migrations/20250816213412_SeedRole.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace api.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedRole : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "22b9c66d-b715-4763-8b8c-7361486be3e8", null, "User", "USER" },
+                    { "7a59df22-b4c3-4a08-81da-02007ec48149", null, "Admin", "ADMIN" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "22b9c66d-b715-4763-8b8c-7361486be3e8");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "7a59df22-b4c3-4a08-81da-02007ec48149");
+        }
+    }
+}
