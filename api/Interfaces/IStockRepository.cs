f1fe5fc1643539c4ebb5b7258efadc245c644964
Hashim332 <hashim.rafiq332@gmail.com>
2025-08-17 22:22:01 +0100

    Lesson 27: Portfolio create - allows users to add stocks to portfolio

--- api/Interfaces/IStockRepository.cs
+++ api/Interfaces/IStockRepository.cs
@@ -12,6 +12,7 @@ namespace api.Interfaces
     {
         Task<List<Stock>> GetAllAsync(QueryObject query);
         Task<Stock?> GetByIdAsync(int id); // FirstOrDefault can be NULL
+        Task<Stock?> GetStockBySymbol(string symbol);
         Task<Stock> CreateAsync(Stock stockModel);
         Task<Stock?> UpdateAsync(int id, UpdateStockRequestDto stockDto);
         Task<Stock?> DeleteAsync(int id);
